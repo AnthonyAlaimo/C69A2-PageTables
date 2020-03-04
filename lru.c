923615fe2a128c72f7b0ddd193ca0023ef432c77
Anthony Alaimo <haikyuuguy@Anthonys-MacBook-Pro-2.local>
2020-03-03 19:00:50 -0500

    fixed bugs in LRU implementation

--- lru.c
+++ lru.c
@@ -16,40 +16,61 @@ extern struct frame *coremap;
 typedef struct linked_list{
 	int frame;
 	struct linked_list *next_node;
+	struct linked_list *prev_node;
 }node;
 
 node *head;
+node *tail;
 /* Page to evict is chosen using the accurate LRU algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 
 int lru_evict() {
+	// int toEvict;
+	// node *curr = head;
+	// node *prev =  NULL;
+	// //vframe = head->frame
+	// // SIZE = 0
+	// if (head == NULL){
+	// 	// do something
+	// 	perror("No head");
+	// }
+	// // Size == 1
+	// if(curr-> next_node == NULL){
+	// 	head = NULL;
+	// 	toEvict = curr->frame;
+	// 	free(curr);
+	// 	return toEvict;
+	// } 
+	// // Size > 1
+	// while (curr->next_node != NULL){
+	// 	prev = curr;
+	// 	curr = curr->next_node;
+	// }
+	// prev->next_node = NULL; 
+	// toEvict = curr->frame;
+	// free(curr);
+	// return toEvict;
 	int toEvict;
-	node *curr = head;
-	node *prev =  NULL;
-	//vframe = head->frame
-	// SIZE = 0
-	if (head == NULL){
+	if (tail == NULL){
 		// do something
-		perror("No head");
-	}
-	// Size == 1
-	if(curr-> next_node == NULL){
-		head = NULL;
-		toEvict = curr->frame;
-		free(curr);
+		if (head == NULL){
+			perror("No nodes");
+			exit(1);
+		}
+		toEvict = head->frame;
+		free(head);
 		return toEvict;
-	} 
-	// Size > 1
-	while (curr->next_node != NULL){
-		prev = curr;
-		curr = curr->next_node;
+		
 	}
-	prev->next_node = NULL; 
-	toEvict = curr->frame;
-	free(curr);
-	return toEvict;
+	node *evictTail = tail;
+	tail = evictTail->prev_node;
+	tail->next_node = NULL;
+	toEvict = evictTail->frame;
+	free(evictTail);
+	return(toEvict);
+
 }
 
 /* This function is called on each access to a page to update any information
@@ -72,6 +93,8 @@ void lru_ref(pgtbl_entry_t *p) {
 	node *new = (node*)malloc(sizeof(node));
 	new->frame = f;
 	new->next_node = head;
+	head->prev_node = new;
+	new->prev_node = NULL;
 	head = new;
 	return;
 }
@@ -82,4 +105,5 @@ void lru_ref(pgtbl_entry_t *p) {
  */
 void lru_init() {
 	head = NULL;
+	tail = NULL;
 }
